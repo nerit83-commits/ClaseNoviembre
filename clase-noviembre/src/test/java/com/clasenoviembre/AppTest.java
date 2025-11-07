83b15127f89cc417dfcfda11bd05473a65da533c
nerit83-commits <nerit83@gmail.com>
2025-11-06 22:11:38 -0300

    Initial commit: Add Maven Java project with .gitignore

---
+++ clase-noviembre/src/test/java/com/clasenoviembre/AppTest.java
@@ -0,0 +1,14 @@
+package com.clasenoviembre;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+    
+public class AppTest {
+ 
+    @Test
+    void testSuma() {
+        int resultado = 2 + 3;
+        assertEquals(5, resultado);
+    }
+
+}
